0ebc9459182a7d20d8c7a5d67315e997ebf36437
OlhaV <olga4irkova@gmail.com>
2016-10-24 18:23:02 +0300

    fix

--- src_backup/js/4pattern.js
+++
@@ -1,9 +0,0 @@
-var patterns = {
-	'name' : /^[a-zA-Z-а-яА-ЯёЁ'(\s)?]+$/,
-	'phone' : /^\+[3][8]\([0][1-9]{2}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/,
-	'email' : /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/
-}
-
-patterns.get = function(sPatternsName) {
-	return patterns[sPatternsName]; 
-} 
